b4425fe640e183f83b62b665a5f80b2458ace1c1
Igor Kostyuchenok <kostyuchenok@fidor.de>
2016-09-14 16:56:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Chav
+`chav` is a hapi.js plugin to generate static routes from json files
---
+++ test/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Code = require('code');
+const Lab = require('lab');
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+const before = lab.before;
+
+const createServer = function (options, callback) {
+
+  const server = new Hapi.Server({
+    debug: false
+  });
+
+  server.connection();
+
+  server.register({
+    register: require('../'),
+    options
+  }, (err) => {
+
+      callback(err, server);
+  });
+};
